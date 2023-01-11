5788ed04091649ebc66c13f06a3ebcd2058e34d1
Иксан <iksan168@abv.bg>
2023-01-11 14:29:37 +0200

    Update DayOfWeek.js

--- SyntaxFunctionsAndStatements/DayOfWeek.js
+++ SyntaxFunctionsAndStatements/DayOfWeek.js
@@ -1,5 +1,5 @@
 function dayOfWeek(day){
-    const weekDays = {"Monday":1,"Tuesday":2,"Wednesday":3,"Thursday":4,"Friday":5,"Saturday":6,"Sunday":7};
+    const weekDays = {'Monday':1,'Tuesday':2,'Wednesday':3,'Thursday':4,'Friday':5,'Saturday':6,'Sunday':7};
     for(weekDay in weekDays){
         if(weekDay == day){
             return console.log(weekDays[weekDay]);
