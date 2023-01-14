9e4bc8dce523df90a48c37604323e7bc826b9e5f
Иксан <iksan168@abv.bg>
2023-01-14 14:26:15 +0200

    Solved exercises

---
+++ ArraysAndNestedArrays/SortAnArrayBy2Criteria.js
@@ -0,0 +1,21 @@
+function solve(array){
+        array.sort((a,b) => {
+        if(a.length > b.length){
+            return 1;
+        }
+        else if(a.length < b.length){
+            return -1;
+        }
+        else{
+            return a.localeCompare(b);
+        }
+    });
+    console.log(array.join('\n'));
+}
+
+
+solve(['test', 
+'Deny', 
+'omen', 
+'Default']
+);
--- ArraysAndNestedArrays/SortingNumbers.js
+++ ArraysAndNestedArrays/SortingNumbers.js
@@ -14,5 +14,3 @@ function solve(array){
     return result;
 }
 solve([1, 65, 3, 52, 48, 63, 31, -3, 18, 56,22]);
-
--3,1,3
