7623d19d6ea8d23e0197dee7dcb265893cf97a6c
Иксан <iksan168@abv.bg>
2023-01-19 19:14:20 +0200

    Create CalorieObject.js

---
+++ ObjectsAndComposition/CalorieObject.js
@@ -0,0 +1,8 @@
+function solve(array){
+    let result = {};
+    for(let i = 0; i < array.length-1; i+=2){
+        result[array[i]] = Number(array[i+1]);
+    }
+    return result;
+}
+console.log(solve(['Yoghurt', '48', 'Rise', '138', 'Apple', '52']));
