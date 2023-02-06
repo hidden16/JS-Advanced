5b9ecbe7485e36d86d8de1fd453de57a2ae67b61
Иксан <iksan168@abv.bg>
2023-02-06 20:58:46 +0200

    Update ColorizeTable.js

--- DOMIntroduction/05.Colorize Table/ColorizeTable.js
+++ DOMIntroduction/05.Colorize Table/ColorizeTable.js
@@ -1,3 +1,9 @@
 function colorize() {
-    // TODO
+    const test = document.getElementsByTagName('tr');
+    const array = Array.from(test);
+    for (let index = 1; index < array.length; index++) {
+        if(index % 2 == 1){
+            test[index].style.backgroundColor = 'Teal';
+        }
+    }
 }
