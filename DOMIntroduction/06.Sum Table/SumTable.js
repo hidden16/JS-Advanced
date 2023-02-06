f83f19e19fadb0d165b83e78e584e259848c483c
Иксан <iksan168@abv.bg>
2023-02-06 21:05:27 +0200

    Update SumTable.js

--- DOMIntroduction/06.Sum Table/SumTable.js
+++ DOMIntroduction/06.Sum Table/SumTable.js
@@ -1,3 +1,9 @@
 function sumTable() {
-
+    let element = document.getElementsByTagName('tr');
+    const array = Array.from(element);
+    let sum = 0;
+    for (let index = 1; index < array.length; index++) {
+        sum += Number(element[index].lastChild.textContent)
+    }
+    document.getElementById('sum').textContent = sum;
 }
