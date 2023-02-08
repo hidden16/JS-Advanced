1245c7f818348232cbcde4ff99b723a7a7562cc4
Иксан <iksan168@abv.bg>
2023-02-08 22:35:25 +0200

    Update search.js

--- DOMIntroduction/Exercise/04. Search in List/search.js
+++ DOMIntroduction/Exercise/04. Search in List/search.js
@@ -1,3 +1,16 @@
 function search() {
-   // TODO
+   let towns = document.getElementsByTagName('li');
+   const search = document.getElementById('searchText').value;
+   let matches = 0;
+   for (let index = 0; index < towns.length; index++) {
+      if (towns[index].textContent.includes(search)) {
+         towns[index].style.fontWeight = 'bold';
+         towns[index].style.textDecoration = 'underline';
+         matches++;
+      }else{
+         towns[index].style.fontWeight = '';
+         towns[index].style.textDecoration = '';
+      }
+   }
+   document.getElementById('result').innerHTML = `${matches} matches found`;
 }
