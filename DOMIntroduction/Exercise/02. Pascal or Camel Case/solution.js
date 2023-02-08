bf4effcfe930429b4c277d38ae0bde7362d690d1
Иксан <iksan168@abv.bg>
2023-02-08 18:44:20 +0200

    Update solution.js

--- DOMIntroduction/Exercise/02. Pascal or Camel Case/solution.js
+++ DOMIntroduction/Exercise/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,20 @@
 function solve() {
-  //TODO...
+  let text = Array.from(document.getElementById('text').value.toLowerCase().split(' '));
+  const convention = document.getElementById('naming-convention').value
+  let conventionType = -1;
+  let result = '';
+  if(convention === 'Camel Case'){
+    conventionType = 1;
+    result += text[0];
+  } else if(convention === 'Pascal Case'){
+    conventionType = 0
+  } else{
+    document.getElementById('result').innerHTML = '<p>Error!</p>';
+    return;
+  }
+  for (let index = conventionType; index < text.length; index++) {
+         result += text[index][0].toUpperCase();
+         result += text[index].slice(1);
+  }
+  document.getElementById('result').innerHTML = `<p>${result}</p>`;
 }
