5daaa37d49288530d3806a457a93533a227f277d
Иксан <iksan168@abv.bg>
2023-02-08 19:42:28 +0200

    Update accordion.js

--- DOMIntroduction/Exercise/03. Accordion/accordion.js
+++ DOMIntroduction/Exercise/03. Accordion/accordion.js
@@ -1,3 +1,10 @@
 function toggle() {
-    console.log('TODO:...');
+    let btnText = document.getElementsByClassName('button')[0];
+    if(btnText.textContent == 'More'){
+        document.getElementById('extra').style.display = 'block';
+        btnText.textContent = 'Less';
+    } else if(btnText.textContent == 'Less'){
+        document.getElementById('extra').style.display = 'none';
+        btnText.textContent = 'More';
+    }
 }
