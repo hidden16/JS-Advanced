9e80e5d82ffc49a35a70f54604c6eb2676fcc082
Иксан <iksan168@abv.bg>
2023-02-09 17:50:36 +0200

    Update solution.js

--- DOMIntroduction/Exercise/05. Table - Search Engine/solution.js
+++ DOMIntroduction/Exercise/05. Table - Search Engine/solution.js
@@ -2,7 +2,14 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
-
+      let text = document.getElementsByTagName('tr');
+      const searchBy = document.getElementById('searchField').value;
+      for (let i = 1; i < text.length; i++) {
+         if(text[i].textContent.includes(searchBy)){
+               text[i].className = 'select'
+         }else{
+            text[i].className = '';
+         }
+      }
    }
 }
