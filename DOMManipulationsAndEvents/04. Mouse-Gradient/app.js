2e3265669e00e39545f4b9a1b3752d67b774a1e0
Иксан <iksan168@abv.bg>
2023-02-13 20:29:34 +0200

    Update app.js

--- DOMManipulationsAndEvents/04. Mouse-Gradient/app.js
+++ DOMManipulationsAndEvents/04. Mouse-Gradient/app.js
@@ -1,3 +1,9 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    const gradient = document.getElementById('gradient');
+    gradient.addEventListener('mousemove', mouseOver);
+
+    function mouseOver(event){
+        const percentage = Math.floor(event.offsetX / 3);
+        document.getElementById('result').textContent = percentage + '%';
+    }
 }
