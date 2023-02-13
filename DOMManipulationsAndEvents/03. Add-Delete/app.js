c422bb69f676cfafe8d1d47aa33506ad4aa5293b
Иксан <iksan168@abv.bg>
2023-02-13 20:17:25 +0200

    Update app.js

--- DOMManipulationsAndEvents/03. Add-Delete/app.js
+++ DOMManipulationsAndEvents/03. Add-Delete/app.js
@@ -1,3 +1,14 @@
 function addItem() {
-    //TODO...
+    const text = document.getElementById('newItemText');
+    const li = document.createElement('li');
+    li.textContent = text.value;
+
+    const deleteBtn = document.createElement('a');
+    deleteBtn.href = '#';
+    deleteBtn.textContent = '[Delete]';
+    deleteBtn.addEventListener('click', () => li.remove());
+
+    li.appendChild(deleteBtn);
+
+    document.getElementById('items').appendChild(li);
 }
