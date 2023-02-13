0c7fa75448b32041ccaac2521aa7dd875bb4ae9c
Иксан <iksan168@abv.bg>
2023-02-13 20:02:36 +0200

    Update app.js

--- DOMManipulationsAndEvents/01. List-Of-Items/app.js
+++ DOMManipulationsAndEvents/01. List-Of-Items/app.js
@@ -2,6 +2,5 @@ function addItem() {
     const text = document.getElementById('newItemText');
     const li = document.createElement('li');
     li.textContent = text.value;
-    const ul = document.getElementById('items');
-    ul.appendChild(li);
+    document.getElementById('items').appendChild(li);
 }
