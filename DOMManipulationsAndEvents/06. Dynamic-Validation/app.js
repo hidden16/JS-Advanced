9a796eb0af50de68023f8001c0017f477e4830fb
Иксан <iksan168@abv.bg>
2023-02-13 20:44:19 +0200

    Update app.js

--- DOMManipulationsAndEvents/06. Dynamic-Validation/app.js
+++ DOMManipulationsAndEvents/06. Dynamic-Validation/app.js
@@ -1,3 +1,13 @@
 function validate() {
-    console.log('TODO:...');
+    const regex = new RegExp('[a-z]+@[a-z]+.[a-z]+');
+    const email = document.getElementById('email');
+    email.addEventListener('change', validateEmail);
+    function validateEmail(){
+        const match = regex.exec(email.value);
+        if (match == null){
+            email.className = 'error';
+        }else{
+            email.className = '';
+        }
+    }
 }
