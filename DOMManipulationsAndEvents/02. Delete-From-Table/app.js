7b46a370e2473aab51b88c275fa4a12939a34eee
Иксан <iksan168@abv.bg>
2023-02-13 20:11:21 +0200

    Update app.js

--- DOMManipulationsAndEvents/02. Delete-From-Table/app.js
+++ DOMManipulationsAndEvents/02. Delete-From-Table/app.js
@@ -1,3 +1,11 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    const items = Array.from(document.querySelectorAll("tbody tr td"));
+    const text = document.querySelector('input[name="email"]').value;
+    const foundItem = items.find(item => item.textContent === text);
+    if(foundItem != undefined){
+        foundItem.parentElement.remove();
+        document.getElementById('result').textContent = 'Deleted.';
+        return;
+    }
+    document.getElementById('result').textContent = 'Not found.';
 }
